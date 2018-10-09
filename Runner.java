490112cf0868d2f40ece21be6eb07afdbcee41d6
evanballinger <ballinger.evan@gmail.com>
2018-10-09 11:55:25 -0600

    first commit with strategy logic for four coin game

--- Runner.java
+++ Runner.java
@@ -4,12 +4,20 @@ public class Runner {
 		int NUMBER_OF_COINS = 4;
 		int REVEALS_PER_TURN = 2;
 		int MAX_NUM_TURNS = 10;
-		String REVEAL_RESULT = "-H-T-";
+		String REVEAL_RESULT_1 = "HH--";
+		
+		String REVEAL_RESULT_2 = "HT--";
+		
+		String REVEAL_RESULT_3 = "TT--";
 		
 		StrategicPlayer myPlayer = new Player();
 		myPlayer.beginGame(NUMBER_OF_COINS,REVEALS_PER_TURN,MAX_NUM_TURNS);
 		System.out.println("player reveals these slots: " + myPlayer.getSlotsToReveal() + "\n");
-		System.out.println("new coin states from player: " + myPlayer.getNewCoinStates(REVEAL_RESULT) + "\n");
+		System.out.println("new coin states from player: " + myPlayer.getNewCoinStates(REVEAL_RESULT_1) + "\n");
+		
+		System.out.println("new coin states from player: " + myPlayer.getNewCoinStates(REVEAL_RESULT_2) + "\n");
+		
+		System.out.println("new coin states from player: " + myPlayer.getNewCoinStates(REVEAL_RESULT_3) + "\n");
 		
 	}
 
