0e87ce37a60c3a0cfe303df889570c4ca8758138
evanballinger <ballinger.evan@gmail.com>
2018-10-23 13:09:07 -0600

    Added Dr. Paul's Ant framework (#4)
    * took out some system print statements
    
    * added single test, moved test resources to lib filoder, added some javadoc comments, need to add instructions for compiling and running tests
    
    * Update README.md
    
    * Update README.md
    
    * Create Requirements.md
    
    * Added ant build framework from Dr. Paul
    
    * modified to match upstream

---
+++ package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides classes that support the
+ * spin-the-wheel coin matching project
+ * and the development of implementations
+ * of the StrategicPlayer interface.
+ * <p>
+ * @author Dr. Jody Paul
+ * @version Fall 2018
+ */
